9b79354f4b961edd66d573724ecfb17030e07157
JJTM20 <78734717+JJTM20@users.noreply.github.com>
2023-10-02 09:10:24 +0200

    Delete P7Internet.Frontend/react-app directory

--- P7Internet.Frontend/react-app/src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-// Her tilføjes alle exports til app.js fra components mappen
-
-export { default as OwnedIngredientsList } from './OwnedIngredientsList';
-export { default as GenerateButton } from './GenerateButton';
