af01f04cc0f183c5023c4ecedfee800673891a41
Casper Søgård <casper-s@hotmail.dk>
2023-10-02 12:19:50 +0200

    Updates to color in layout

--- P7Internet.Frontend/react-app/src/components/ContainerUpper.jsx
+++ P7Internet.Frontend/react-app/src/components/ContainerUpper.jsx
@@ -5,7 +5,7 @@ const ContainerUpper = () => {
 
   return (
     <div className='ContainerUpper'>
-        <h1>Recipe Generator</h1>
+        <h1 style={{"text-align": "center"}}>Recipe Generator</h1>
         <h2>Owned Ingredients</h2>
         <SearchBar/>
 
