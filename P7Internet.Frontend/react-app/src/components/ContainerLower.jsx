a4959850cf4f983bc9f8ecfbd8f28ecd2d5b6ddf
SebxGit <minhseb@gmail.com>
2023-10-04 11:36:51 +0200

    arrows can be used to see multiple recipes

--- P7Internet.Frontend/react-app/src/components/ContainerLower.jsx
+++ P7Internet.Frontend/react-app/src/components/ContainerLower.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {RecipeTitle} from '.'
 import SelectArrows from './SelectArrows'
+import leftArrow from '../data/leftArrow.svg'
+import rightArrow from '../data/rightArrow.svg'
 
 const ContainerLower = () => {
-  const Ingredients = ['400g Chicken Breast', '400g Asparagus', '3dl Water', '1.5dl Milk']
 	const ownedList = ['Chicken Breast', 'Salt', 'Water', 'Milk']
+
+  const Ingredients = [
+    ['400g Chicken Breast', '400g Asparagus', '3dl Water', '1.5dl Milk'],
+    ['400g Chicken Breast', '400g Asparagus', '3dl Water'],
+    ['400g Chicken Breast', '400g Asparagus'],
+    ['400g Chicken Breast']
+  ];
+
+  const [tab, setTab] = useState(0)
+
+  const clickLeft = () => {setTab(Math.max(0,tab-1))}
+  const clickRight = () => {setTab(Math.min(tab+1,Ingredients.length-1))}
 
   function filterOwned(){
     let filtered = [];
-    for (let i in Ingredients){
-      const ingr = Ingredients[i].substring(Ingredients[i].indexOf(' ')+1)
-      if (ownedList.includes(ingr)) filtered.push(Ingredients[i])
+    for (let i in Ingredients[tab]){
+      const ingr = Ingredients[tab][i].substring(Ingredients[tab][i].indexOf(' ')+1)
+      if (ownedList.includes(ingr)) filtered.push(Ingredients[tab][i])
     }
     return filtered;
   }
@@ -18,11 +31,15 @@ const ContainerLower = () => {
   return (
     <div className='ContainerLower'>
         <RecipeTitle/>
-        <ul>{Ingredients.map(ingredient => <li>{ingredient} 
+        <ul>{Ingredients[tab].map(ingredient => <li>{ingredient} 
         {filterOwned().includes(ingredient) ? ' Owned' : ''}
         </li>)}</ul>
         <button>Select</button>
-        <SelectArrows/>
+        
+        <div className='SelectArrows'>
+          <img src={leftArrow} alt='Left Arrow' onClick={clickLeft}/>
+          <img src={rightArrow} alt='right Arrow' onClick={clickRight}/>
+        </div>
     </div>
   )
 }
