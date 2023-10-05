92e65b982fe918add2c530466c5d54ba90ac7b5b
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-10-05 14:26:30 +0200

    Cleaned up some CSS. Number field buttons have validation. Number field is read only

--- P7Internet.Frontend/react-app/src/components/NumberField.jsx
+++ P7Internet.Frontend/react-app/src/components/NumberField.jsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 const NumberField = () => {
 
   //Set the intial value of the number field 
-  const [value, setValue] = useState(69);
+  const [value, setValue] = useState(4);
 
   //Set the min and max values of the number field
   const minValue = 1;
-  const maxValue = 100;
+  const maxValue = 10;
 
   
   //Every time the user changes the numberfield we check if the value is within the expected values
@@ -28,11 +28,17 @@ const NumberField = () => {
   }
 
   function add(){
-    setValue(value + 1);
+    if (value < maxValue)
+      setValue(value + 1);
+    else 
+      alert("maximum is 100");
   }
 
   function subtract(){
-    setValue(value - 1);
+    if(value > minValue)
+      setValue(value - 1);
+    else
+      alert("minimum is 1");
   }
 
   return (
@@ -44,6 +50,7 @@ const NumberField = () => {
         size={3}
         value={value}
         onChange={updateNumberField}
+        readOnly
       />
       <button class="plusMinusButton" onClick={add}>+</button>
     </div>
