0424add227e8b268dae7d4e0d0798d7d49ad77dd
Casper Søgård <casper-s@hotmail.dk>
2023-10-10 11:12:12 +0200

    Added React Names

--- P7Internet.Frontend/react-app/src/components/DietaryRestrictions.jsx
+++ P7Internet.Frontend/react-app/src/components/DietaryRestrictions.jsx
@@ -7,27 +7,27 @@ const DietaryRestrictions = () => {
       <div id="DietaryRestrictionsCheckbox">
         <div>
           <input className="DietaryRestrictionsCheckboxSize" type="checkbox" name="Pescitarian" />
-          <label for="Pescitarian">Pescitarian</label>
+          <label htmlFor="Pescitarian">Pescitarian</label>
         </div>
 
         <div>
           <input className="DietaryRestrictionsCheckboxSize" type="checkbox" name="Vegan" />
-          <label for="Vegan">Vegan</label>
+          <label htmlFor="Vegan">Vegan</label>
         </div>
 
         <div>
           <input className="DietaryRestrictionsCheckboxSize" type="checkbox" name="Vegetarian" />
-          <label for="Vegetarian">Vegetarian</label>
+          <label htmlFor="Vegetarian">Vegetarian</label>
         </div>
 
         <div>
           <input className="DietaryRestrictionsCheckboxSize" type="checkbox" name="Lactose free" />
-          <label for="Lactose free">Lactose free</label>
+          <label htmlFor="Lactose free">Lactose free</label>
         </div>
 
         <div>
           <input className="DietaryRestrictionsCheckboxSize" type="checkbox" name="Gluten free" />
-          <label for="Gluten free">Gluten free</label>
+          <label htmlFor="Gluten free">Gluten free</label>
         </div>
       </div>
     </div>
