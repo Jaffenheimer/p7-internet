a7be845750656fca01bf58cbabfa1981f2b8a978
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-10-10 10:27:50 +0200

    Removed function UpdateNumberField

--- P7Internet.Frontend/react-app/src/components/NumberField.jsx
+++ P7Internet.Frontend/react-app/src/components/NumberField.jsx
@@ -11,21 +11,6 @@ const NumberField = () => {
   const minValue = 1;
   const maxValue = 10;
 
-  
-  //Every time the user changes the numberfield we check if the value is within the expected values
-  function UpdateNumberField(event){
-    if (event.target.value < minValue) {
-      setValue(minValue);
-      alert("minimum is 1");
-    }
-    else if (event.target.value > maxValue) {
-        setValue(maxValue);
-        alert("maximum is 100");
-    }
-    else {
-        setValue(event.target.value);
-    }
-  }
 
   function Add(){
     if (value < maxValue)
@@ -49,7 +34,6 @@ const NumberField = () => {
         type="number"
         size={3}
         value={value}
-        onChange={UpdateNumberField}
         readOnly
       />
       <button class="PlusMinusButton" onClick={Add}>+</button>
