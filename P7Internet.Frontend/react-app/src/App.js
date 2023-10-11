0190c23dd3c493876be9c6b976486e7645953d87
lildhansen <madslh@yahoo.com>
2023-10-11 16:22:20 +0200

    routing is set up og har ændret lidt på strukturen, og jeg tager det kinda i mig igen, fuck react

--- P7Internet.Frontend/react-app/src/App.js
+++ P7Internet.Frontend/react-app/src/App.js
@@ -1,40 +1,25 @@
+import React from 'react'
 import './App.css';
-import Example from './components/Example';
-import {ContainerUpper, ContainerLower, ContainerRight} from './components'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
+import FullRecipeView from "./pages/FullRecipeView";
+import FrontPage from "./pages/FrontPage";
 
-function App() {
 
+
+export default function App() {
   axios.defaults.baseURL="http://localhost:5000";
-
-  return (
-    <div className="App">
-      <div className='AppLeft'>
-        <ContainerUpper/>
-        <ContainerLower/>
-      </div>
-      <div className='AppRight'>
-        <ContainerRight/>
-        <p>Svar fra API</p>
-        <Example />
-      </div>
-    </div>
+  
+  return (    
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<FrontPage />}/>
+          <Route path="FullRecipeView" element={<FullRecipeView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
 
 //husk også at ændre i './components/index.jsx'
-
-  // const [element, setElement] = useState('');
-  // useEffect(() => {
-  //   fetch('https://localhost:5001/admin/sample/ingredients')
-  //     .then((response) => response.json())
-  //     .then((data => {
-  //         console.log(data.title);
-  //         setElement(data);
-  //     }))
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     })
-  // })
---
+++ P7Internet.Frontend/react-app/src/pages/FullRecipeView.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function FullRecipeView() {
+  return (
+    <div>FullRecipeView
+      <h1>h12</h1>
+    </div>
+  )
+}
+
+export default FullRecipeView
