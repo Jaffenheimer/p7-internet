f9e2a866b8ca4e06d5b9e1dd563ba5b9ba5b4002
Casper Søgård <casper-s@hotmail.dk>
2023-10-12 00:30:37 +0200

    Added reducer to recipeGenerationSlice

--- P7Internet.Frontend/react-app/src/features/recipeSlice.js
+++ P7Internet.Frontend/react-app/src/features/recipeSlice.js
@@ -1,9 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    ownedlist: [], 
-}
+    ownedIngredients: [],
+    dietaryRestrictions: [], 
+    excludeList: [],
+};
 
-export const recipeSlice = createSlice({
-    name: ownedList,
+export const recipeGenerationSlice = createSlice({
+    name: 'recipeGeneration', 
+    initialState, 
+    reducers: {
+        addOwnedIngredients(state, action){
+            state.ownedIngredients.push(action.payload); 
+        }, 
+        removeOwnedIngreidents(state, action){
+            state.ownedIngredients = state.ownedIngredients.filter(ingredient => ingredient.id !== action.payload);
+        }, 
+        selectDietaryRestrictions(state, action){
+            state.dietaryRestrictions.push(action.payload);
+        }, 
+        deselectDietaryRestrictions(state, action){
+            state.dietaryRestrictions = state.ownedIngredients.filter(dietaryRestrictions => dietaryRestrictions !== action.payload);
+        }, 
+        addExcludedIngredient(state, action){
+            state.excludeList.push(action.payload); 
+        }, 
+        removeExcludedIngredient(state, action){
+            state.excludeList = state.excludeList.filter(ingredient => ingredient.id !== action.payload);
+        },
+    },  
 }); 
+
+export const {actions: recipeGenerationActions, reducer: recipeGenerationreducer} = recipeGenerationSlice
