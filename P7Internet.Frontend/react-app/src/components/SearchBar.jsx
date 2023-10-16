35afb7de92ad568b608b40858da3ce868e4191cd
SebxGit <minhseb@gmail.com>
2023-10-16 15:23:41 +0200

    removed inputRef after merge

--- P7Internet.Frontend/react-app/src/components/SearchBar.jsx
+++ P7Internet.Frontend/react-app/src/components/SearchBar.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-  const inputRef = useRef();
   const [ingredient, setIngredient] = useState('');
   const ownedIngredientsList = useSelector(state => state.recipeGeneration.ownedIngredients);
 
@@ -13,25 +12,24 @@ const SearchBar = () => {
   };
 
   const handleSubmit = (event) => {
-      event.preventDefault();
-      inputRef.current.value = '';
-      if(ingredient !== null && typeof(ingredient) !== 'undefined'){
-        if (ingredient == "") return
+    event.preventDefault();
+    if(ingredient !== null && typeof(ingredient) !== 'undefined'){
+      if (ingredient == "") return
 
-        // receives the ingredient text (aka. name) from dict on store in format 
-        // {0:{id: '', text: ''}, 1:{id: '', text: ''}}¨
-        var test = Object.values(ownedIngredientsList)
-        var ownedIngredientText = []
+      // receives the ingredient text (aka. name) from dict on store in format 
+      // {0:{id: '', text: ''}, 1:{id: '', text: ''}}¨
+      var test = Object.values(ownedIngredientsList)
+      var ownedIngredientText = []
 
-        test.forEach((ownedIngredient) => ownedIngredientText.push(ownedIngredient['text']))
+      test.forEach((ownedIngredient) => ownedIngredientText.push(ownedIngredient['text']))
 
-        // only adds to ownedIngredient if non-dublicate
-        if (!ownedIngredientText.includes(ingredient))
-          dispatch(recipeGenerationActions.addOwnedIngredients(ingredient));
-        else
-          alert(`Elementet "${ingredient}" er allerede tilføjet til listen!`)
-      }
-      setIngredient(event.target.value); 
+      // only adds to ownedIngredient if non-dublicate
+      if (!ownedIngredientText.includes(ingredient))
+        dispatch(recipeGenerationActions.addOwnedIngredients(ingredient));
+      else
+        alert(`Elementet "${ingredient}" er allerede tilføjet til listen!`)
+    }
+    setIngredient(''); 
   }; 
 
   return (
