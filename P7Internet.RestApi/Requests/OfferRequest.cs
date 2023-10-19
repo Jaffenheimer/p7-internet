3d3696b80e33342e5d4497887c7bd73ed0117bc5
JJTM20 <jjtm20@student.aau.dk>
2023-10-19 09:17:16 +0200

    Calculates geohash from zip

--- P7Internet.RestApi/Requests/OfferRequest.cs
+++ P7Internet.RestApi/Requests/OfferRequest.cs
@@ -6,38 +6,36 @@ namespace P7Internet.Requests
 {
     public class OfferRequest
     {
-        public int Zip { get; }
+        public int Zip { get; set; }
         public int Pagesize { get; set; }
         public string SearchTerm { get; set; }
         public int Radius { get; set; }
         public string Upcoming { get; set; }
-        public string Geohash { get; set; }
         public OfferRequest()
         {
 
         }
 
-        public OfferRequest(int zip, int pagesize, string searchTerm, int radius, string upcoming, string geoHash)
+        public OfferRequest(int zip, int pagesize, string searchTerm, int radius, string upcoming)
         {
             Zip = zip;
             Pagesize = pagesize;
             SearchTerm = searchTerm;
             Radius = radius;
             Upcoming = upcoming;
-            Geohash = geoHash;
         }
 
-        public string ComposeOfferObject()
+        public string ComposeOfferObject(string geohash)
         {
             string res = $@"{{
             ""page"": {{""page_size"": {this.Pagesize}
               }},
              ""where"": {{
-                ""term"": ""{this.SearchTerm}"",
-                ""max_radius"": {this.Radius},
-                ""include_upcoming"": {this.Upcoming.ToLower()}
+                ""term"": ""{SearchTerm}"",
+                ""max_radius"": {Radius},
+                ""include_upcoming"": {Upcoming.ToLower()}
              }},
-             ""geohash"": ""{this.Geohash}""
+             ""geohash"": ""{geohash}""
              }}";
 
             return res;
--- P7Internet.RestApi/Services/EtilbudsAvisService.cs
+++ P7Internet.RestApi/Services/EtilbudsAvisService.cs
@@ -4,13 +4,13 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
-using System.Text.Json;
 using Microsoft.AspNetCore.Http.Extensions;
 using P7Internet.CustomExceptions;
 using P7Internet.Requests;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using SharedObjects;
+using Geohash;
 
 namespace P7Internet.Services;
 
@@ -27,12 +27,11 @@ public class ETilbudsAvisService
 
     public async Task<IList<Offer>> GetAllOffers(OfferRequest req)
     {
+        string geohash;
         try
         {
-            //if (zip == 0 || zip == null) { throw new ZipNotFoundException(); }
-            //var coords = await GetCoordinates(zip);
-            //_queryBuilder.Add("r_lng", coords["lon"]);
-            //_queryBuilder.Add("r_lat", coords["lat"]);
+            if (req.Zip == 0 || req.Zip == null) { throw new ZipNotFoundException(); }
+            geohash = await CalculateGeohashFromZip(req.Zip);
         }
         catch (Exception)
         {
@@ -41,10 +40,10 @@ public class ETilbudsAvisService
         
         var url = new Uri(_client.BaseAddress.ToString());
         var request = new HttpRequestMessage(HttpMethod.Post, url);
+
         request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         request.Headers.Add("X-Api-Key", "EFSiDV");
-        StringContent payload = new StringContent(req.ComposeOfferObject(), System.Text.Encoding.UTF8, "application/json");
-            
+        StringContent payload = new StringContent(req.ComposeOfferObject(geohash), System.Text.Encoding.UTF8, "application/json");
 
         request.Content = payload;
         var response = await _client.SendAsync(request);
@@ -53,6 +52,7 @@ public class ETilbudsAvisService
 
         deserializedContent.Remove("page_info");
         var offerArray = deserializedContent.Value<JArray>("offers");
+
         //This creates the offer objects from the parsed json data.
         IList<Offer> offers = new List<Offer>();
         for (int i = 0; i < offerArray.Count; i++)
@@ -74,29 +74,21 @@ public class ETilbudsAvisService
 
 
     #region Utility Functions
-    public async Task<Dictionary<string, string>> GetCoordinates(int postalCode)
+    public async Task<string> CalculateGeohashFromZip(int zip)
     {
+        var geohasher = new Geohash.Geohasher();
         var coordinateClient = new HttpClient() { BaseAddress = new Uri("https://api.dataforsyningen.dk/postnumre/") };
-        var url = new Uri(coordinateClient.BaseAddress, postalCode.ToString());
+        var url = new Uri(coordinateClient.BaseAddress, zip.ToString());
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         var response = await coordinateClient.SendAsync(request);
         var jsonData = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
-        var coordinates = new Dictionary<string, string>() { { "lon", jsonData["visueltcenter"][0].ToString() }, { "lat", jsonData["visueltcenter"][1].ToString() } };
-        return coordinates;
+        var coordinates = new Dictionary<string, double>() { { "lon", (double)jsonData["visueltcenter"][0] }, { "lat", (double)jsonData["visueltcenter"][1] } };
+        var geohash = geohasher.Encode(coordinates["lat"], coordinates["lon"]);
+        return geohash;
     }
     public T CreateObjectFromDeserializedJson<T>(JObject jsonObject)
     {
         return jsonObject.ToObject<T>();
     }
     #endregion
-}
-
-//[JsonObject(MemberSerialization.OptIn)]
-[JsonObject(MemberSerialization.OptIn)]
-public class Store
-{
-    [JsonProperty("name")]
-    public string Name;
-    [JsonProperty("logo")]
-    public string Logo;
 }
