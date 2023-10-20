51013bce82f951a087c83f0a9ee81a04250a5f02
lildhansen <madslh@yahoo.com>
2023-10-20 09:21:13 +0200

    store selection also uses react-Select now

--- P7Internet.Frontend/react-app/src/components/StoreSelection.jsx
+++ P7Internet.Frontend/react-app/src/components/StoreSelection.jsx
@@ -1,25 +1,42 @@
 import React from "react";
-import stores from "../objects/Stores";
 import { storesSlice } from "../features/storesSlice";
 import { useDispatch } from "react-redux";
 import { storesActions } from "../features/storesSlice";
+import Select from "react-select";
 
 const StoreSelection = () => {
   const dispatch = useDispatch();
 
-  const handleChecked = (event) => {
-    const name = event.target.name;
-    //Strips whitespace
-    const strippedValue = name.replace(/ +/g, "");
-    //convert to lower case
-    const loweredValue = strippedValue.toLowerCase();
-    dispatch(storesActions.toggleStore(loweredValue));
+  const stores = [
+    {value: "Bilka",label: "Bilka"},
+    {value: "Rema 1000",label: "Rema 1000"},
+    {value: "Netto",label: "Netto"},
+    {value: "Føtex",label: "Føtex"},
+    {value: "Kvickly",label: "Kvickly"},
+    {value: "Fakta",label: "Fakta"},
+    {value: "Lidl",label: "Lidl"},
+  ];
+
+
+
+  const handleChange = (event) => {
+    const stores = [];
+    for (const element of event) {
+      const value = element.value.toLowerCase();
+      stores.push(value);
+    }
+    dispatch(storesActions.setStores(stores));
   };
 
   return (
     <div className="StoreSelection">
       <h3 id="StoreSelectionText">Mulige Butikker:</h3>
-      {/* todo */}
+      <Select
+        isMulti
+        id="storesSelect"
+        options={stores}
+        onChange={handleChange}
+      />
     </div>
   );
 };
