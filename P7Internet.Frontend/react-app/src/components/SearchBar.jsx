22f12354ffe7388c93832aa94407d6a2bd9955ce
lildhansen <madslh@yahoo.com>
2023-10-23 10:05:13 +0200

    did some groundwork for disabling and fixed conflicts when merging other work into it

--- P7Internet.Frontend/react-app/src/components/SearchBar.jsx
+++ P7Internet.Frontend/react-app/src/components/SearchBar.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { recipeGenerationActions } from "../features/recipeGenerationSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
-  const [ingredient, setIngredient] = useState("");
-  const ownedIngredientsList = useSelector(
-    (state) => state.recipeGeneration.ownedIngredients
-  );
+  const [ingredient, setIngredient] = useState('');
+  const ownedIngredientsList = useSelector(state => state.recipeGeneration.ownedIngredients);
+  const [addButtonIsDisabled, setAddButtonDisabled] = useState(true);
+  const [removeAllButtonIsDisabled, setRemoveAllButtonDisabled] = useState(true);
+
+  console.log(ingredient)
+
+  useEffect(() => {
+    handleRemoveAllButtonDisabling();}
+    , []);
 
   const handleChange = (event) => {
     setIngredient(event.target.value);
+    handleAddButtonDisabling(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -33,12 +40,30 @@ const SearchBar = () => {
       else alert(`Elementet "${ingredient}" er allerede tilføjet til listen!`);
     }
     setIngredient("");
+    handleRemoveAllButtonDisabling();
   };
 
   function submitRemoveAll() {
     dispatch(recipeGenerationActions.clearAllOwnedIngredients());
   }
 
+  //add button is disabled if input is empty, else enabled
+  const handleAddButtonDisabling = (value) => {
+    if (value === "")
+      setAddButtonDisabled(true)
+    else
+      setAddButtonDisabled(false)
+  }
+
+  //remove all button is disabled if there are no ingredients to remove, else enabled
+  const handleRemoveAllButtonDisabling = () => {
+    //console.log(ownedIngredientsList)
+    if (ownedIngredientsList.length === 0)
+      setRemoveAllButtonDisabled(true)
+    else
+      setRemoveAllButtonDisabled(false)
+  }
+    
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -48,8 +73,9 @@ const SearchBar = () => {
         onChange={handleChange}
         placeholder="Tilføj en ingrediens..."
       />
-      <button type="submit">Tilføj</button>
-      <button id="RemoveAllExcludeIngredientsButton" onClick={submitRemoveAll}>
+      <button type="submit" disabled={addButtonIsDisabled} >Tilføj</button>
+      <button id="RemoveAllExcludeIngredientsButton" disabled={removeAllButtonIsDisabled}
+      onClick={submitRemoveAll}>
         Fjern alle
       </button>
     </form>
