96e9b658cd89d21580c1d73bfb930f7dafa937e6
JJTM20 <jjtm20@student.aau.dk>
2023-10-24 11:20:20 +0200

    Can get geolocation of user and saves it to browsers localStorage

--- P7Internet.Frontend/react-app/src/components/GenerateRecipeButton.jsx
+++ P7Internet.Frontend/react-app/src/components/GenerateRecipeButton.jsx
@@ -3,46 +3,61 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { pageActions } from "../features/pageSlice";
 import Pages from "../objects/Pages";
-
+import toast from "react-hot-toast";
 
 const GenerateRecipeButton = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    getGeoLocation();
 
-  function goToPageFullRecipeSelection() {
-    dispatch(pageActions.goToPage(Pages.RecipeSelection));
-  }
-
-  //handles all the logic for when the button is clicked
-  function handleOnClick() {
-    GenerateRecipesHandler();
-    goToPageFullRecipeSelection();
-  }
-
-  //insert comment about what the function does here
-  const GenerateRecipesHandler = async () => {
-
-    const req = "Create 3 short and simple recipes for 4 people";
-
-    try {
-      const response = await axios.post(
-        "/public/sample/testrecipes",
-        req,
-        {
-          params: {
-            req,
-          },
-        },
-        {
-          "Access-Control-Allow-Origin": "*",
-        }
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log(error.message);
+    function goToPageFullRecipeSelection() {
+        dispatch(pageActions.goToPage(Pages.RecipeSelection));
     }
-  };
 
-  return <button onClick={handleOnClick}>Generer opskrifter</button>;
+    //handles all the logic for when the button is clicked
+    function handleOnClick() {
+        GenerateRecipesHandler();
+        goToPageFullRecipeSelection();
+    }
+
+    //insert comment about what the function does here
+    const GenerateRecipesHandler = async () => {
+        const req = "Create 3 short and simple recipes for 4 people";
+
+        try {
+            const response = await axios.post(
+                "/public/sample/testrecipes",
+                req,
+                {
+                    params: {
+                        req,
+                    },
+                },
+                {
+                    "Access-Control-Allow-Origin": "*",
+                }
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    return <button onClick={handleOnClick}>Generer opskrifter</button>;
 };
+
+function getGeoLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            localStorage.setItem(
+                "geolocation",
+                JSON.stringify({
+                    lat: pos.coords.latitude,
+                    lon: pos.coords.longitude,
+                    acc: pos.coords.accuracy,
+                })
+            );
+        });
+    } else toast("Geolocation not supported");
+}
 
 export default GenerateRecipeButton;
