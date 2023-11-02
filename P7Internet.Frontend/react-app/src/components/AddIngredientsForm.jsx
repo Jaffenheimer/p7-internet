453a3b6b50e9090128adb9416d3debd222648bb4
lildhansen <madslh@yahoo.com>
2023-11-02 12:09:55 +0100

    fixed warning

--- P7Internet.Frontend/react-app/src/components/AddIngredientsForm.jsx
+++ P7Internet.Frontend/react-app/src/components/AddIngredientsForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import RemoveAllButton from "./RemoveAllButton";
 import AddIngredientInput from "./AddIngredientInput";
