3f1ab813799ff2c7f07b52ebbc610a4b3d62d486
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2023-11-06 15:06:39 +0100

    Moved front page button into a component and did some styling

---
+++ P7Internet.Frontend/react-app/src/components/FrontPageButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { pageActions } from "../features/pageSlice";
+import Pages from "../objects/Pages";
+import "react-toastify/dist/ReactToastify.css";
+
+const FrontPageButton = () => {
+  const dispatch = useDispatch();
+
+  function goToPageFrontPage() {
+    dispatch(pageActions.goToPage(Pages.frontPage));
+  }
+  return (
+    <button id="BackToFrontPageButton" onClick={goToPageFrontPage}>
+      Tilbage til forsiden
+    </button>
+  );
+};
+
+export default FrontPageButton;
--- P7Internet.Frontend/react-app/src/components/RecipeSelectionContainerLeft.jsx
+++ P7Internet.Frontend/react-app/src/components/RecipeSelectionContainerLeft.jsx
@@ -2,29 +2,20 @@ import React from "react";
 import RecipeView from "./RecipeView";
 import SelectArrows from "./SelectArrows";
 import SelectRecipeButton from "./SelectRecipeButton";
-import { useDispatch } from "react-redux";
-import { pageActions } from "../features/pageSlice";
-import Pages from "../objects/Pages";
+import FrontPageButton from "./FrontPageButton";
 
 const RecipeSelectionContainerLeft = () => {
   function enableArrowKeyPress() {
     document.getElementById("SelectArrows").focus();
   }
 
-  const dispatch = useDispatch();
-
-  function goToPageFrontPage() {
-    dispatch(pageActions.goToPage(Pages.frontPage));
-  }
   return (
     <div className="RecipeSelectionContainerLeft" onClick={enableArrowKeyPress}>
       <RecipeView />
       <div style={{ position: "relative" }}>
         <SelectRecipeButton />
+        <FrontPageButton />
         <SelectArrows />
-        <button id="BackButton" onClick={goToPageFrontPage}>
-          Tilbage
-        </button>
       </div>
     </div>
   );
