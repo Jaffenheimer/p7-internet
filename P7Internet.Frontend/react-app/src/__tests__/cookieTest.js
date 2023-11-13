78c230732eb1ba319110224b2d13f43f54ca951e
Casper Søgård <casper-s@hotmail.dk>
2023-11-13 01:32:55 +0100

    Added test for cookiehandler

--- P7Internet.Frontend/react-app/src/__tests__/cookieTest.js
+++ P7Internet.Frontend/react-app/src/__tests__/cookieTest.js
@@ -1,9 +1,30 @@
-import retriveCookie from "../helperFunctions/retriveCookie";
+import { addCookie, deleteCookies, retriveCookie } from "../helperFunctions/cookieHandler";
 
 test("Does retriveCookie retrive the cookie correctly", () => {
   const testUsername = "username";
   document.cookie = `username=${testUsername};`;
-  const username = retriveCookie("username=");
-  expect(username).toBe("username");
+  const retrivedUsername = retriveCookie("username=");
+  expect(retrivedUsername).toBe("username");
+
+  //Deleting cookie
   document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+});
+
+test("Can Cookies be added", () => {
+  const testUsername = "username";
+  const testUserId = "userid";
+  const testUserSessionToken = "sessiontoken";
+
+  addCookie(testUsername, testUserId, testUserSessionToken); 
+
+  const retrivedUsername = retriveCookie("username=");
+  const retrivedUserId = retriveCookie("userid=");
+  const retrivedUserSessionToken = retriveCookie("sessionToken=");
+
+  expect(retrivedUsername).toBe("username");
+  expect(retrivedUserId).toBe("userid");
+  expect(retrivedUserSessionToken).toBe("sessiontoken");
+
+  //Deleting cookies
+  deleteCookies();
 });
