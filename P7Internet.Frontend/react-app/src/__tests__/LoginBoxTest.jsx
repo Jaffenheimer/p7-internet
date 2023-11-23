913af001805e1cbc79e5ace557df67d6cd0d525d
SebxGit <72699667+sebxGit@users.noreply.github.com>
2023-11-23 10:33:31 +0100

    ASD

--- P7Internet.Frontend/react-app/src/__tests__/LoginBoxTest.jsx
+++ P7Internet.Frontend/react-app/src/__tests__/LoginBoxTest.jsx
@@ -1,47 +1,45 @@
-// import { cleanup, fireEvent, screen  } from "@testing-library/react";
+import { cleanup, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import React from "react";
-// import {renderComponent} from "../testSetupHelper/Helper.jsx";
-// import LoginBox from "../components/LoginBox";
+import { renderComponent } from "../testSetupHelper/Helper.jsx";
+import LoginBox from "../components/LoginBox";
 
-test("", () => {});
+afterEach(cleanup);
 
-// afterEach(cleanup)
+test("Renders the expected text in login box", () => {
+  renderComponent(<LoginBox />);
+  expect(screen.getByText(/Brugernavn/)).toBeInTheDocument();
+  expect(screen.getByText(/Kodeord/)).toBeInTheDocument();
+  expect(screen.getByText(/Ingen bruger:/)).toBeInTheDocument();
+});
 
-// test("Renders the expected text in login box", () => {
-//   renderComponent(<LoginBox />)
-//   expect(screen.getByText(/Brugernavn/)).toBeInTheDocument();
-//   expect(screen.getByText(/Kodeord/)).toBeInTheDocument();
-//   expect(screen.getByText(/Ingen bruger:/)).toBeInTheDocument();
-// });
+test("Renders the expected image in login box", () => {
+  renderComponent(<LoginBox />);
+  const image = screen.getByRole("img");
+  expect(image).toHaveAttribute("src", "cross.svg");
+});
 
-// test("Renders the expected image in login box", () => {
-//   renderComponent(<LoginBox />)
-//   const image = screen.getByRole('img')
-//   expect(image).toHaveAttribute('src', 'cross.svg');
-// });
+test("Renders the expected button in login box", () => {
+  renderComponent(<LoginBox />);
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
+});
 
-// test("Renders the expected button in login box", () => {
-//   renderComponent(<LoginBox />)
-//   const button = screen.getByRole('button');
-//   expect(button).toBeInTheDocument();
-// });
+test("Renders the cross image in login box and it is clickable", () => {
+  const mockCallBack = jest.fn(() => {});
+  renderComponent(<LoginBox closeModal={mockCallBack} />);
+  const image = screen.getByRole("img");
+  expect(image).toBeInTheDocument();
+  fireEvent.click(image);
+  expect(mockCallBack.mock.calls).toHaveLength(1);
+});
 
-// test("Renders the cross image in login box and it is clickable", () => {
-//   const mockCallBack = jest.fn(() => {})
-//   renderComponent(<LoginBox closeModal={mockCallBack}/>)
-//   const image = screen.getByRole('img');
-//   expect(image).toBeInTheDocument();
-//   fireEvent.click(image);
-//   expect(mockCallBack.mock.calls).toHaveLength(1);
-// });
+test("Render links and change url on click", () => {
+  renderComponent(<LoginBox />);
+  const changePasswordLink = screen.getByLabelText("glemtKodeord");
+  expect(changePasswordLink).toBeInTheDocument();
+  expect(changePasswordLink).toHaveAttribute("href", "/#");
 
-// test("Render links and change url on click", () => {
-//   renderComponent(<LoginBox />)
-//   const changePasswordLink = screen.getByLabelText('glemtKodeord');
-//   expect(changePasswordLink).toBeInTheDocument();
-//   expect(changePasswordLink).toHaveAttribute('href', '/#');
-
-//   // fireEvent.click(changePasswordLink);
-//   // expect(global.window.location.href).toContain('http://localhost/')
-// })
+  // fireEvent.click(changePasswordLink);
+  // expect(global.window.location.href).toContain('http://localhost/')
+});
