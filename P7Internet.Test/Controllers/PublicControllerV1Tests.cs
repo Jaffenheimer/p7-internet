eba580973a333000eda8d20ccb3600685c96afc0
JJTM20 <jjtm20@student.aau.dk>
2023-12-12 09:01:42 +0100

    All tests pass locally. GH actions please be gentle

--- P7Internet.Test/Controllers/PublicControllerV1Tests.cs
+++ P7Internet.Test/Controllers/PublicControllerV1Tests.cs
@@ -255,7 +255,7 @@ namespace P7Internet.Test.Controllers
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(new Offer());
 
             //Act
-            IActionResult actionResult = controller.GetOffer(new OfferRequest(1, "Kylling", 5000, "true")).Result;
+            IActionResult actionResult = controller.GetOffer(new OfferRequest(1, "Kylling", 5000, "true", It.IsAny<string>())).Result;
             var contentResult = actionResult as OkObjectResult;
 
             //Assert
@@ -269,7 +269,7 @@ namespace P7Internet.Test.Controllers
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(value: null);
 
             //Act
-            IActionResult actionResult = controller.GetOffer(new OfferRequest(1, "Kylling", 5000, "true")).Result;
+            IActionResult actionResult = controller.GetOffer(new OfferRequest(1, "Kylling", 5000, "true", It.IsAny<string>())).Result;
             var contentResult = actionResult as OkObjectResult;
 
             //Assert
@@ -281,13 +281,13 @@ namespace P7Internet.Test.Controllers
         public void GetOfferFromEtilbudsavisSuccess()
         {
             //Arrange
-            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true");
+            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true", "Bilka, Føtex, Netto");
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(value: null);
             _cachedOfferRepositoryMock
                 .Setup(x => x.UpsertOffer(It.IsAny<string>(), It.IsAny<decimal>(), It.IsAny<string>()))
                 .ReturnsAsync(true);
             _etilbudsAvisServiceMock.Setup(x => x.GetAllOffers(offerRequest))
-                .ReturnsAsync(new List<Offer>() {new Offer(), new Offer()});
+                .ReturnsAsync(new List<Offer>() {new Offer() { Store = "Bilka"}, new Offer()});
 
             //Act
             IActionResult actionResult = controller.GetOffer(offerRequest).Result;
@@ -305,7 +305,7 @@ namespace P7Internet.Test.Controllers
         public void GetOfferFromEtilbudsavisFail()
         {
             //Arrange
-            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true");
+            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true", It.IsAny<string>());
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(value: null);
             _etilbudsAvisServiceMock.Setup(x => x.GetAllOffers(offerRequest)).ReturnsAsync(value: null);
 
@@ -325,7 +325,7 @@ namespace P7Internet.Test.Controllers
         public void GetOfferFromSallingSuccessIfNotFoundInCacheOrEtilbudsavisSuccess()
         {
             //Arrange
-            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true");
+            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true", It.IsAny<string>());
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(value: null);
             _cachedOfferRepositoryMock
                 .Setup(x => x.UpsertOffer(It.IsAny<string>(), It.IsAny<decimal>(), It.IsAny<string>()))
@@ -351,7 +351,7 @@ namespace P7Internet.Test.Controllers
         public void GetOfferFromSallingSuccessIfNotFoundInCacheOrEtilbudsavisFail()
         {
             //Arrange
-            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true");
+            var offerRequest = new OfferRequest(1, "Kylling", 5000, "true", It.IsAny<string>());
             _cachedOfferRepositoryMock.Setup(x => x.GetOffer(It.IsAny<string>())).ReturnsAsync(value: null);
             _iIngredientRepositoryMock.Setup(x => x.GetAllIngredients()).Returns(Task.FromResult(new List<string>()
                 {"Æble, Kartoffel, Julebryg, Fløde, Vegansk"}));
